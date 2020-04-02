253625ee6abc34b8183f0e4a4755c826e546f1fd
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-02 15:38:00 +0530

    feat: add __manifest__.py file

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Hospital Management',
+    'version': '12.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for managing Hospitals',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'Sumesh Majhi',
+    'maintainer': 'MajhiRockzZ',
+    'website': 'https://www.majhirockzz.me/',
+    'depends': [],
+    'demo': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
